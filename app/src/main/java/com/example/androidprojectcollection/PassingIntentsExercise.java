55cdb401d3d2ca3334ca7d4c41e9cd7949356fdd
GrandKou <rieub123@gmail.com>
2024-04-28 21:59:15 +0800

    born to travel the isekai world, forced to real life problems

--- app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise.java
+++ app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise.java
@@ -14,35 +14,79 @@ public class PassingIntentsExercise extends AppCompatActivity {
 
     EditText Fname;
     EditText Lname;
-    EditText MFname;
-    EditText MLname;
-    EditText FFname;
-    EditText FLname;
     RadioButton male;
     RadioButton female;
     RadioButton others;
     EditText bDate;
     EditText emailAdd;
     EditText PhoneNum;
-    EditText Address;
-    EditText Course;
+    EditText Program;
     EditText YrLevel;
+    EditText StudentID;
+    EditText HomeAddress;
+    EditText Nationality;
     Button Submit;
     Button Clear;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passing_intents_exercise);
 
+        Fname = (EditText) findViewById(R.id.txtFname);
+        Lname = (EditText) findViewById(R.id.txtLname);
+        bDate = (EditText) findViewById(R.id.txtBDate);
+        emailAdd = (EditText) findViewById(R.id.txtEmail);
+        PhoneNum = (EditText) findViewById(R.id.numNumber);
+        Program = (EditText) findViewById(R.id.txtProgram);
+        YrLevel = (EditText) findViewById(R.id.txtYearLvl);
+        StudentID = (EditText) findViewById(R.id.txtStudID);
+        HomeAddress = (EditText) findViewById(R.id.txtAddress);
+        Nationality = (EditText) findViewById(R.id.txtNation);
+        male = (RadioButton) findViewById(R.id.radMale);
+        female = (RadioButton) findViewById(R.id.radFemale);
+        others = (RadioButton) findViewById(R.id.radOthers);
+
+
         Submit = (Button) findViewById(R.id.btnSumbit);
         Submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(
-                        PassingIntentsExercise.this, PassingIntentsExercise2.class
-                );
+                String fname = Fname.getText().toString();
+                String lname = Lname.getText().toString();
+                String BDate = bDate.getText().toString();
+                String Email = emailAdd.getText().toString();
+                String Phonenum = PhoneNum.getText().toString();
+                String HomeAdd = HomeAddress.getText().toString();
+                String program = Program.getText().toString();
+                String year_level = YrLevel.getText().toString();
+                String stdID = StudentID.getText().toString();
+                String nation = Nationality.getText().toString();
+
+                String gender;
+                if(male.isChecked()) {
+                    gender = "Male";
+                } else if (female.isChecked()) {
+                    gender = "Female";
+                } else if(others.isChecked()) {
+                    gender = "Others";
+                } else {
+                    gender = "Unknown";
+                }
+
+                Intent intent = new Intent(PassingIntentsExercise.this, PassingIntentsExercise2.class);
+                intent.putExtra("fname_key", fname);
+                intent.putExtra("lname_key",  lname);
+                intent.putExtra("gender_key",  gender);
+                intent.putExtra("birth_date_key",  BDate);
+                intent.putExtra("email_add_key",  Email);
+                intent.putExtra("phone_num_key",  Phonenum);
+                intent.putExtra("home_add_key",  HomeAdd);
+                intent.putExtra("course_key",  program);
+                intent.putExtra("year_lvl_key",  year_level);
+                intent.putExtra("student_id",  stdID);
+                intent.putExtra("nationality",  nation);
+
                 startActivity(intent);
             }
         });
@@ -53,65 +97,18 @@ public class PassingIntentsExercise extends AppCompatActivity {
             public void onClick(View view) {
                 Fname.setText("");
                 Lname.setText("");
-                MFname.setText("");
-                MLname.setText("");
-                FFname.setText("");
-                FLname.setText("");
                 emailAdd.setText("");
                 PhoneNum.setText("");
-                Address.setText("");
-                Course.setText("");
+                Program.setText("");
                 bDate.setText("");
                 YrLevel.setText("");
+                StudentID.setText("");
+                HomeAddress.setText("");
+                Nationality.setText("");
                 male.setSelected(false);
                 female.setSelected(false);
                 others.setSelected(false);
             }
         });
-    }
-    public void onClick(View v) {
-        String fname = Fname.getText().toString();
-        String lname = Lname.getText().toString();
-
-        String mfname = MFname.getText().toString();
-        String mlname = MLname.getText().toString();
-
-        String ffname = FFname.getText().toString();
-        String flname = FLname.getText().toString();
-
-        String gender;
-        if(male.isChecked()) {
-            gender = "Male";
-        } else if (female.isChecked()) {
-            gender = "Female";
-        } else if(others.isChecked()) {
-            gender = "Others";
-        } else {
-            gender = "Unknown";
-        }
-
-        String BDate = bDate.getText().toString();
-        String Email = emailAdd.getText().toString();
-        String Phonenum = PhoneNum.getText().toString();
-        String HomeAdd = Address.getText().toString();
-        String course = Course.getText().toString();
-        String year_level = YrLevel.getText().toString();
-
-        Intent intent = new Intent(PassingIntentsExercise.this, PassingIntentsExercise2.class);
-            intent.putExtra("fname_key", fname);
-            intent.putExtra("lname_key",  lname);
-            intent.putExtra("Mfname_key",  mfname);
-            intent.putExtra("Mlname_key",  mlname);
-            intent.putExtra("Ffname_key",  ffname);
-            intent.putExtra("Flname_key",  flname);
-            intent.putExtra("gender_key",  gender);
-            intent.putExtra("birth_date_key",  BDate);
-            intent.putExtra("email_add_key",  Email);
-            intent.putExtra("phone_num_key",  Phonenum);
-            intent.putExtra("home_add_key",  HomeAdd);
-            intent.putExtra("course_key",  course);
-            intent.putExtra("year_lvl_key",  year_level);
-
-            startActivity(intent);
     }
 }
--- app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise2.java
+++ app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise2.java
@@ -2,13 +2,78 @@ package com.example.androidprojectcollection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PassingIntentsExercise2 extends AppCompatActivity {
+
+    TextView Name;
+    TextView Gender;
+    TextView Birthdate;
+    TextView PhoneNumber;
+    TextView Email;
+    TextView Program;
+    TextView StudentID;
+    TextView YearLevel;
+    TextView Address;
+    TextView Nationality;
+    Button btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passing_intents_exercise2);
+
+        Intent intent = getIntent();
+
+        Name = findViewById(R.id.Name);
+        Gender = findViewById(R.id.Gender);
+        Birthdate = findViewById(R.id.Birthdate);
+        PhoneNumber = findViewById(R.id.PhoneNum);
+        Email = findViewById(R.id.Email);
+        Program = findViewById(R.id.Program);
+        StudentID = findViewById(R.id.StdID);
+        YearLevel = findViewById(R.id.YrLevel);
+        Address = findViewById(R.id.Address);
+        Nationality = findViewById(R.id.Nationality);
+
+        String fname = intent.getStringExtra("fname_key");
+        String lname = intent.getStringExtra("lname_key");
+        String fullname = fname + " " + lname;
+        String gender = intent.getStringExtra("gender_key");
+        String BDate = intent.getStringExtra("birth_date_key");
+        String email = intent.getStringExtra("email_add_key");
+        String Phonenum = intent.getStringExtra("phone_num_key");
+        String HomeAdd = intent.getStringExtra("home_add_key");
+        String program = intent.getStringExtra("course_key");
+        String year_level = intent.getStringExtra("year_lvl_key");
+        String stdID = intent.getStringExtra("student_id");
+        String nation = intent.getStringExtra("nationality");
+
+        Name.setText(fullname);
+        Gender.setText(gender);
+        Birthdate.setText(BDate);
+        PhoneNumber.setText(Phonenum);
+        Email.setText(email);
+        Program.setText(program);
+        StudentID.setText(stdID);
+        YearLevel.setText(year_level);
+        Address.setText(HomeAdd);
+        Nationality.setText(nation);
+
+        btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        PassingIntentsExercise2.this, PassingIntentsExercise.class
+                );
+                startActivity(intent);
+            }
+        });
     }
 }
